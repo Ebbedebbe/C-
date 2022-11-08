77d331cc959013df15193574a58013f3fd4307f3
Ebbedebbe <82200620+Ebbedebbe@users.noreply.github.com>
2022-11-08 13:58:57 +0100

    Add files via upload

---
+++ Fortnite2/Fortnite2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+class Person
+{
+    string Name = "ggdeeznuts";
+    string Email = "gg@nutsies.com";
+    string Country = "Timor Leste";
+    int Age = 4;
+   
+
+    public bool JoinTourney()
+    {
+        return true;
+    }
+
+    public double Salary()
+    {
+        double vejgaarddollar = 50.5; // inflation
+        Console.WriteLine(vejgaarddollar);
+        return vejgaarddollar;
+    }
+}
+
+class Lvl100MafiaBoss : Person
+{
+    public void dialogue()
+    {
+        Console.WriteLine("Lvl100MafiaBoss: ...");
+    }
+}
+
+class Lvl1Crook : Person
+{
+    public void dialogue()
+    {
+        Console.WriteLine("Lvl1Crook: Just use code Mafia2022 for 100.000 Silver!");
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        Person Lvl100MafiaBoss = new Person();
+
+        Lvl100MafiaBoss Boss = new Lvl100MafiaBoss();
+
+        Lvl1Crook Crook = new Lvl1Crook();
+
+        Crook.dialogue();
+        Boss.dialogue();
+    }
+}
